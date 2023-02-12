577d55d3b0177c18fe0378f0bb9e4e45b775d83a
PriestOfFerns <timofeydenin@gmail.com>
2023-02-12 20:28:02 +0300

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,117 +1,13 @@
+local basalt = require("basalt")
 
+local main = basalt.createFrame()
+local button = main --> Basalt returns an instance of the object on most methods, to make use of "call-chaining"
+        :addButton() --> This is an example of call chaining
+        :setPosition(4,4) 
+        :setText("Click me!")
+        :onClick(
+            function() 
+                basalt.debug("I got clicked!") 
+            end)
 
-local cobalt = dofile("/cobalt/init.lua")
-
-args = {...}
-
-
-
-local monitor = peripheral.wrap(args[1] or "monitor_3")
-local input = peripheral.wrap(args[2] or "minecraft:barrel_3")
-local storage = peripheral.wrap(args[3] or "create:item_vault_0")
-
-local Buttons = {}
-
-local Stage = 0
-
-term.redirect(monitor)
-
-term.clear()
-
-local function Start()
-    Stage=1
-end
-
-local function Back()
-    Stage = 0
-end
-
-local function InvCheck()
-    local DCount = 0
-    for slot, item in pairs(input.list()) do
-        
-        if item.name == "minecraft:diamond" then
-            DCount=DCount+item.count
-        end
-    end
-    return DCount
-end
-
-
-function cobalt.draw()
-    term.redirect(term.native())
-    print(Stage)
-    
-        term.redirect(monitor)
-    if Stage == 0 then
-        cobalt.graphics.setBackgroundColor("green")
-        cobalt.graphics.print("BLACKJACK",2,2)
-        cobalt.graphics.setColor("brown")
-        cobalt.graphics.setBackgroundColor("green")
-        cobalt.graphics.rect("line",0,0,29,18)
-        
-        
-
-        cobalt.graphics.setBackgroundColor("brown")
-        cobalt.graphics.setColor("white")
-        cobalt.graphics.print("Start",11,11)
-
-        Buttons = { {x={10,17},y={10,12},func=Start} }
-
-        cobalt.graphics.setBackgroundColor("green")
-    elseif Stage == 1 then
-        
-
-        cobalt.graphics.setColor("black")
-        
-        
-        
-
-        cobalt.graphics.setBackgroundColor("black")
-        cobalt.graphics.setColor("white")
-        cobalt.graphics.print("Back",11,11)
-
-
-        
-        cobalt.graphics.setColor("cyan")
-
-        local DCount = 0
-        for slot, item in pairs(input.list()) do
-            
-            if item.name == "minecraft:diamond" then
-                DCount=DCount+item.count
-            end
-        end
-
-        local text = "Diamonds: "..tostring(DCount)
-        cobalt.graphics.print(text,11,5)
-
-
-        Buttons = { {x={10,17},y={10,12},func=Back} }
-        cobalt.graphics.setBackgroundColor("white")
-    end
-
-
-   
-end
-
-function cobalt.mousepressed(x, y, button)
-    term.redirect(term.native())
-    print(tostring(x)..":"..tostring(y))
-
-    term.redirect(monitor)
-    
-    if  #Buttons > 0 then
-        for k,v in pairs(Buttons) do
-            if x >= v.x[1] and x <= v.x[2] and y >= v.y[1] and y <= v.y[2] then
-                v.func()
-            end
-        end
-    end
-    
-end
-
-
-
--- Start Cobalt's cycle
-cobalt.init()
+basalt.autoUpdate()
