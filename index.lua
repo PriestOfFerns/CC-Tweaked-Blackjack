c4b298486ff6e55933316b278585f37a4e8102c4
PriestOfFerns <timofeydenin@gmail.com>
2023-02-12 04:04:00 +0300

    Update index.lua

--- index.lua
+++ index.lua
@@ -7,8 +7,8 @@ args = {...}
 
 
 local monitor = peripheral.wrap(args[1] or "monitor_3")
-local barrel1 = peripheral.wrap(args[2] or "minecraft:barrel_2")
-local barrel2 = peripheral.wrap(args[3] or "minecraft:barrel_3")
+local input = peripheral.wrap(args[2] or "minecraft:barrel_2")
+local storage = peripheral.wrap(args[3] or "create:item_vault_0")
 
 local Buttons = {}
 
@@ -26,6 +26,17 @@ local function Back()
     Stage = 0
 end
 
+local function InvCheck()
+    local DCount = 0
+    for slot, item in pairs(input.list()) do
+        if item.name == "minecraft:Diamond" then
+            DCount=DCount+item.count
+        end
+    end
+    return DCount
+end
+
+
 function cobalt.draw()
     if Stage == 0 then
         cobalt.graphics.setBackgroundColor("green")
@@ -38,9 +49,9 @@ function cobalt.draw()
 
         cobalt.graphics.setBackgroundColor("brown")
         cobalt.graphics.setColor("white")
-        cobalt.graphics.print("Start",11,11)
+        cobalt.graphics.print("Start",11,8)
 
-        Buttons = { {x={10,18},y={9,13},func=Start} }
+        Buttons = { {x={10,17},y={7,9},func=Start} }
 
         cobalt.graphics.setBackgroundColor("green")
     elseif Stage == 1 then
@@ -53,10 +64,15 @@ function cobalt.draw()
 
         cobalt.graphics.setBackgroundColor("black")
         cobalt.graphics.setColor("white")
-        cobalt.graphics.print("Back",11,11)
+        cobalt.graphics.print("Back",11,8)
 
-        Buttons = { {x={10,18},y={9,13},func=Back} }
 
+        
+        cobalt.graphics.setColor("cyan")
+        cobalt.graphics.print("Diamonds: "..tostring(InvCheck()),11,5)
+
+
+        Buttons = { {x={10,17},y={7,9},func=Back} }
         cobalt.graphics.setBackgroundColor("white")
     end
 
