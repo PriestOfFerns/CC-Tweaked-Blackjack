6044eafb32100cf9c5676ad64ecd71f623fb155c
PriestOfFerns <timofeydenin@gmail.com>
2023-02-12 20:33:52 +0300

    Added Basalt

--- index.lua
+++ index.lua
@@ -1,13 +1,6 @@
 local basalt = require("basalt")
 
 local main = basalt.createFrame()
-local button = main --> Basalt returns an instance of the object on most methods, to make use of "call-chaining"
-        :addButton() --> This is an example of call chaining
-        :setPosition(4,4) 
-        :setText("Click me!")
-        :onClick(
-            function() 
-                basalt.debug("I got clicked!") 
-            end)
+main:setBackground(colors.red, "#", colors.yellow)
 
 basalt.autoUpdate()
