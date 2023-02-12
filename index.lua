077726f39d8fedbac466a77d2a97d2f9fb767c66
PriestOfFerns <timofeydenin@gmail.com>
2023-02-12 21:14:39 +0300

    Update index.lua

--- index.lua
+++ index.lua
@@ -3,4 +3,10 @@ local basalt = require("basalt")
 local main = basalt.createFrame()
 main:setBackground(colors.red, "#", colors.yellow)
 
+local title = main:addLabel()
+:SetText("Slots")
+:setFontSize(4)
+:setPosition(2,4)
+
+
 basalt.autoUpdate()
